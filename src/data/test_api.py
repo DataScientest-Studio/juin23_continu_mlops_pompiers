092b3fd315a6eb2d2610baa44a21cabf4cec7ff9
Willy <130974329+WillyMaillot87@users.noreply.github.com>
2023-08-30 13:45:49 +0200

    Update tests pytest

--- src/data/test_api.py
+++ src/data/test_api.py
@@ -2,3 +2,4 @@ from api_v1 import NewCall
 
 import pytest
 
+# Vérification des fonctions de l'API
--- src/data/test_import_raw_data.py
+++ src/data/test_import_raw_data.py
@@ -2,6 +2,7 @@ from import_raw_data import result, columns
 
 import pytest
 
+# Vérifie le nombre de colonnes de la database
 def test_len_columns():
   assert len(columns) == 14
 
