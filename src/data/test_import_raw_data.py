dea0b93f0fc6b7b139488b32d385f6c5c49c3dcc
Willy <130974329+WillyMaillot87@users.noreply.github.com>
2023-09-05 15:35:23 +0200

    Ajout d'un test de connection à la DB

--- src/data/test_import_raw_data.py
+++ src/data/test_import_raw_data.py
@@ -1,6 +1,24 @@
 from import_raw_data import result, columns
 
+import mysql.connector
 import pytest
+
+# Vérifier la connexion à la base de donnée
+def test_database_connection():
+  try:
+      connection = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          password="pompiers",
+          database="london_fire_brigade"
+      )
+      assert connection.is_connected() == True
+  except mysql.connector.Error as err:
+      pytest.fail(f"Erreur de connexion à la base de données : {err}")
+  finally:
+      if connection.is_connected():
+          connection.close()
+
 
 # Vérifie le nombre de colonnes de la database
 def test_len_columns():
