02561f7fcd04733e6d8a1531dec7e3210b896fec
dlabdouni <d.labdouni@gmail.com>
2023-09-21 21:33:11 +0200

    Update fichiers test dans dossier test

---
+++ src/test/test_metrics.py
@@ -0,0 +1,10 @@
+from model import mse_lgb, mae_lgb, r2_lgb, rmse_lgb
+
+import pytest
+
+# Vérification de la performance du modèle LGBM
+def test_r2():
+  assert r2_lgb > 0.5
+
+def test_rmse():
+  assert rmse_lgb < 120
