c8ea662485401133acf9b0b5eca1337f32faa6cd
dlabdouni <d.labdouni@gmail.com>
2023-09-24 21:57:30 +0200

    Remove tests folder at root folder

---
+++ test/test_import_raw_data.py
@@ -0,0 +1,54 @@
+from data.import_raw_data import result, columns, DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
+
+import mysql.connector
+import pytest
+
+
+# Vérifier la connexion à la base de donnée
+def test_database_connection():
+  try:    
+      connection = mysql.connector.connect(
+          host=DB_HOST,
+          user=DB_USER,
+          password=DB_PASSWORD,
+          database=DB_NAME
+      )
+      assert connection.is_connected() == True
+  except mysql.connector.Error as err:
+      pytest.fail(f"Erreur de connexion à la base de données : {err}")
+  finally:
+      if connection.is_connected():
+          connection.close()
+
+
+# Vérifie le nombre de colonnes de la database
+def test_len_columns():
+  assert len(columns) == 14
+
+
+
+
+
+
+
+# Vérifier la connexion à la base de donnée
+def test_database_connection():
+  try:
+      connection = mysql.connector.connect(
+          host=DB_HOST,
+          user=DB_USER,
+          password=DB_PASSWORD,
+          database=DB_NAME
+      )
+      assert connection.is_connected() == True
+  except mysql.connector.Error as err:
+      pytest.fail(f"Erreur de connexion à la base de données : {err}")
+  finally:
+      if connection.is_connected():
+          connection.close()
+
+
+# Vérifie le nombre de colonnes de la database
+def test_len_columns():
+  assert len(columns) == 14
+
---
+++ test/test_metrics.py
@@ -0,0 +1,11 @@
+from models.model import mse, mae, r2_lgb, rmse_lgb
+
+import pytest
+
+# Vérification de la performance du modèle LGBM
+def test_r2():
+  assert r2_lgb > 0.5
+
+def test_rmse():
+
+  assert rmse_lgb < 120
