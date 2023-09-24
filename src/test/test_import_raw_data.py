ebfd55560e345064e8ef2ace332c9fdf6bd6b5d5
dlabdouni <d.labdouni@gmail.com>
2023-09-24 17:07:17 +0200

    Update test_import_raw_data.py

--- src/test/test_import_raw_data.py
+++ src/test/test_import_raw_data.py
@@ -1,16 +1,22 @@
-from import_raw_data import result, columns
+from data.import_raw_data import result, columns
 
 import mysql.connector
 import pytest
+from decouple import config
+
+DB_HOST = config('DB_HOST')
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
 
 # Vérifier la connexion à la base de donnée
 def test_database_connection():
-  try:
+  try:    
       connection = mysql.connector.connect(
-          host="localhost",
-          user="root",
-          password="pompiers",
-          database="london_fire_brigade"
+          host=DB_HOST,
+          user=DB_USER,
+          password=DB_PASSWORD,
+          database=DB_NAME
       )
       assert connection.is_connected() == True
   except mysql.connector.Error as err:
@@ -26,4 +32,31 @@ def test_len_columns():
 
 
 
+
+
+from data.import_raw_data import result, columns
+
+import mysql.connector
+import pytest
+
+# Vérifier la connexion à la base de donnée
+def test_database_connection():
+  try:
+      connection = mysql.connector.connect(
+          host=DB_HOST,
+          user=DB_USER,
+          password=DB_PASSWORD,
+          database=DB_NAME
+      )
+      assert connection.is_connected() == True
+  except mysql.connector.Error as err:
+      pytest.fail(f"Erreur de connexion à la base de données : {err}")
+  finally:
+      if connection.is_connected():
+          connection.close()
+
+
+# Vérifie le nombre de colonnes de la database
+def test_len_columns():
+  assert len(columns) == 14
 
