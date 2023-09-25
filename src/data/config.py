293cbb46d0a5a23b5c311760ee1a6fd3889d02d9
dlabdouni <d.labdouni@gmail.com>
2023-09-25 11:38:36 +0200

    Update config.py

--- src/data/config.py
+++ src/data/config.py
@@ -1,4 +1,6 @@
-DB_HOST = 'host'
-DB_USER = 'test_user'
-DB_PASSWORD = 'test_password'
-DB_NAME = 'test_database'
+from decouple import config
+
+DB_HOST = config('DB_HOST')
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
