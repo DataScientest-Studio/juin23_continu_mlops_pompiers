a306b1e867f2119b446bb42373236ed132c99e4a
dlabdouni <d.labdouni@gmail.com>
2023-09-27 17:36:04 +0200

    Update import_raw_data.py

--- src/data/import_raw_data.py
+++ src/data/import_raw_data.py
@@ -11,7 +11,7 @@ DB_PASSWORD = config('DB_PASSWORD')
 DB_NAME = config('DB_NAME')
 
 connection = mysql.connector.connect(
-        host=DB_HOST
+        host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME
