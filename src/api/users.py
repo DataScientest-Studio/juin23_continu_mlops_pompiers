106169061eb6a9b4094a9409b505ba132b16758e
dlabdouni <d.labdouni@gmail.com>
2023-09-28 12:06:59 +0200

    Update users.py

--- src/api/users.py
+++ src/api/users.py
@@ -2,17 +2,6 @@ from fastapi import status,Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from decouple import config
 
-
-
-#Dictionnaire des identifiants et des mots de passe :
-users_db = {
-    "willy": "Pompiers2023*",
-    "djamel": "pompiers",
-    "jonathan": "pompiers",
-    "root": "pompiers",
-    "admin": "pompiers"
-}
-
 # Fonction pour la vérification des identifiants et mots de passe des utilisateurs.
 def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
     user = credentials.username
@@ -20,11 +9,11 @@ def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic()))
     
     # Chargez les identifiants et les mots de passe à partir des variables d'environnement
     users_db = {
-        "willy": config('USER_DB_WILLY', default=''),
-        "djamel": config('USER_DB_DJAMEL', default=''),
-        "jonathan": config('USER_DB_JONATHAN', default=''),
-        "root": config('USER_DB_ROOT', default=''),
-        "admin": config('USER_DB_ADMIN', default=''),
+        "willy": config('USER_DB_WILLY'),
+        "djamel": config('USER_DB_DJAMEL'),
+        "jonathan": config('USER_DB_JONATHAN'),
+        "root": config('USER_DB_ROOT'),
+        "admin": config('USER_DB_ADMIN'),
     }
 
     if user in users_db and password == users_db[user]:
