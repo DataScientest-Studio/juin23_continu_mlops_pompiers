b43094424109b74d025943b0acb36dc94153e725
dlabdouni <d.labdouni@gmail.com>
2023-09-28 09:57:27 +0200

    Update schema.py

--- src/api/schema.py
+++ src/api/schema.py
@@ -17,3 +17,25 @@ class NewCall(BaseModel):
     PumpOrder: int
     DelayCodeId: int
     Month: int #TODO Ajouter la condition pour que ce soit un entier compris entre 1 et 12
+    
+    
+# Créez une instance de NewCall avec l'heure actuelle et le mois actuel
+from datetime import datetime
+current_hour = datetime.now().hour
+current_month = datetime.now().month
+
+new_call = NewCall(
+    HourOfCall=current_hour,
+    IncGeo_BoroughCode="example",
+    IncGeo_WardCode="example",
+    Easting_rounded=123,
+    Northing_rounded=456,
+    IncidentStationGround="example",
+    NumStationsWithPumpsAttending=1,
+    NumPumpsAttending=2,
+    PumpCount=3,
+    PumpHoursRoundUp=4,
+    PumpOrder=5,
+    DelayCodeId=6,
+    Month=current_month
+)
