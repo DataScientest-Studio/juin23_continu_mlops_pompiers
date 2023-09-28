6f373c145ef8b3bb1390e19f492d14aca4f89000
dlabdouni <d.labdouni@gmail.com>
2023-09-28 11:07:27 +0200

    Update users.py

--- src/api/users.py
+++ src/api/users.py
@@ -1,5 +1,6 @@
 from fastapi import status,Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from decouple import config
 
 
 
@@ -16,6 +17,15 @@ users_db = {
 def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
     user = credentials.username
     password = credentials.password
+    
+    # Chargez les identifiants et les mots de passe à partir des variables d'environnement
+    users_db = {
+        "willy": config('USER_DB_WILLY', default=''),
+        "djamel": config('USER_DB_DJAMEL', default=''),
+        "jonathan": config('USER_DB_JONATHAN', default=''),
+        "root": config('USER_DB_ROOT', default=''),
+        "admin": config('USER_DB_ADMIN', default=''),
+    }
 
     if user in users_db and password == users_db[user]:
         return user
