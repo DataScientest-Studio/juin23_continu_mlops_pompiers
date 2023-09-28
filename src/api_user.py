c130fe28aef040181a5762347f5bdeaa1a3f66f6
dlabdouni <d.labdouni@gmail.com>
2023-09-28 10:08:11 +0200

    Update api_user.py

--- src/api_user.py
+++ src/api_user.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 import uvicorn
 import pandas as pd
 
-from api.schema import newCall
+from api.schema import new_Call
 from api.users import verify_credentials
 from models_training.model import scaler
 import datetime
@@ -50,7 +50,7 @@ async def get_index(current_user: str = Depends(verify_credentials)):
 
 
 @app.post('/predict', tags=['Machine Learning'], name='predictions')
-async def predict(new_call: newCall):
+async def predict(new_call: new_Call):
     """
     Obtenir une prédiction à partir de nouvelles données d'entrée.
     Les données d'entrée doivent être une instance de la class NewCall.
