8a8de3fede8e8a6aebeccf6cf221bfb6352e867b
dlabdouni <d.labdouni@gmail.com>
2023-09-28 14:40:47 +0200

    Update test_api_admin.py

--- src/test_api_admin.py
+++ src/test_api_admin.py
@@ -8,8 +8,8 @@ from decouple import config
 client = TestClient(api)
 
 #login : 
-admin = "willy"
-password = config('PWD_DB_WILLY', default='')
+admin = "admin"
+password = config('PWD_DB_ADMIN', default='')
 
 def test_access_allowed():
     # Fonction de test pour vérifier l'accès autorisé
