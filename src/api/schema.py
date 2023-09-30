c8a70ab50b815a5f737a57e349842b212bff33e7
dlabdouni <d.labdouni@gmail.com>
2023-09-30 12:54:32 +0200

    Update schema.py

--- src/api/schema.py
+++ src/api/schema.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
 
 
+# Créez une instance de NewCall avec l'heure actuelle et le mois actuel
+from datetime import datetime
+current_hour = int(datetime.now().hour)
+current_month = int(datetime.now().month)
+
+
 
 # Classe new_call pour faire une prédiction.
 class NewCall(BaseModel):
-    HourOfCall: int #TODO Ajouter la condition pour que ce soit un entier compris entre 0 et 23
+    HourOfCall: int = current_hour 
     IncGeo_BoroughCode: str
     IncGeo_WardCode: str
     Easting_rounded: int
@@ -16,26 +22,5 @@ class NewCall(BaseModel):
     PumpHoursRoundUp: int
     PumpOrder: int
     DelayCodeId: int
-    Month: int #TODO Ajouter la condition pour que ce soit un entier compris entre 1 et 12
+    Month: int = current_month
     
-    
-# Créez une instance de NewCall avec l'heure actuelle et le mois actuel
-from datetime import datetime
-current_hour = int(datetime.now().hour)
-current_month = int(datetime.now().month)
-
-new_Call = NewCall(
-    HourOfCall=current_hour,
-    IncGeo_BoroughCode="example",
-    IncGeo_WardCode="example",
-    Easting_rounded=123,
-    Northing_rounded=456,
-    IncidentStationGround="example",
-    NumStationsWithPumpsAttending=1,
-    NumPumpsAttending=2,
-    PumpCount=3,
-    PumpHoursRoundUp=4,
-    PumpOrder=5,
-    DelayCodeId=6,
-    Month=current_month
-)
