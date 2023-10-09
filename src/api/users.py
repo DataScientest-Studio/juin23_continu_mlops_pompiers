4a3f4b6dde8ac680218d2a181379b910d3f0178a
dlabdouni <d.labdouni@gmail.com>
2023-10-09 22:07:42 +0200

    Update users.py

--- src/api/users.py
+++ src/api/users.py
@@ -2,12 +2,9 @@ from fastapi import status,Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from decouple import config
 
-# Fonction pour la vérification des identifiants et mots de passe des utilisateurs.
-def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
-    user = credentials.username
-    password = credentials.password
-    
-    users_db = {
+
+#Dictionnaire des identifiants et des mots de passe :
+users_db = {
         "willy": config('PWD_DB_WILLY', default=''),
         "djamel": config('PWD_DB_DJAMEL', default=''),
         "jonathan": config('PWD_DB_JONATHAN', default=''),
@@ -15,10 +12,32 @@ def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic()))
         "admin": config('PWD_DB_ADMIN', default=''),
     }
 
-    if user in users_db and password == users_db[user]:
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Unauthorized",
-        headers={"WWW-Authenticate": "Basic"},
-    )
+# Fonction pour la vérification des identifiants et mots de passe des utilisateurs.
+def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    user = credentials.username
+    password = credentials.password
+    
+
+    if not user or not password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Veuillez vous identifier en fournissant un identifiant et un mot de passe valides",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+   
+    if user in users_db:
+        stored_password = users_db[user]
+        if password == stored_password:
+            return user
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Non autorisé. Mauvais mot de passe",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Non Autorisé. Mauvais identifiant",
+            headers={"WWW-Authenticate": "Basic"},
+        )
