409aa3a59003eca80781b6fc91ee209d8b288268
dlabdouni <d.labdouni@gmail.com>
2023-10-10 15:05:41 +0200

    Update model.py

--- src/models_training/model.py
+++ src/models_training/model.py
@@ -21,10 +21,15 @@ def prepare_data(df) :
     X = df.drop('AttendanceTimeSeconds', axis=1)
     y = df['AttendanceTimeSeconds']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
-    sc = MinMaxScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    return X_train, X_test, y_train, y_test, sc
+    return X_train, X_test, y_train, y_test
+
+def scale_data(X_train, X_test) :
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    dump(scaler, 'models/scaler_fitted.joblib') # Enregistrer le scaler MinMaxScaler() ajusté aux données d'entrainement.
+    return X_train_scaled, X_test_scaled
 
 def train_linear_reg(X_train, y_train):
     '''
@@ -68,32 +73,3 @@ def evaluate_model(model_name, y_test, y_pred):
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
     rmse = np.sqrt(mse)
-
-    # Affichage des métriques
-    print(f"Métriques du modèle {model_name}:")
-    print("Mean Squared Error (MSE): ", mse)
-    print("Mean Absolute Error (MAE): ", mae)
-    print("R-squared (R²): ", r2)
-    print("Root Mean Squared Error (RMSE): ", rmse)
-
-    return mse, mae, r2, rmse
-
-# Préparation des données :
-X_train, X_test, y_train, y_test, scaler = prepare_data(df)
-
-# Entrainement et résultats du modèle de régression linéaire :
-model_lr = train_linear_reg(X_train, y_train)
-y_pred_lr = pred_model(model_lr, X_test)
-mse_lr, mae_lr, r2_lr, rmse_lr = evaluate_model('Régression linéaire', y_test, y_pred_lr)
-
-# Entrainement et résultats du modèle Light GBM :
-model_lgb = train_lightgbm(X_train, y_train)
-y_pred_lgb = pred_model(model_lgb, X_test)
-mse_lgb, mae_lgb, r2_lgb, rmse_lgb = evaluate_model('Light GBM', y_test, y_pred_lgb)
-
-dump(model_lgb, 'models/model_lgb.joblib')
-
-# Entrainement et résultats du modèle Random Forest :
-# model_rf = train_random_forest(X_train, y_train)
-# y_pred_rf = pred_model(model_rf, X_test)
-# mse_rf, mae_rf, r2_rf, rmse_rf = evaluate_model('Random Forest', y_test, y_pred_rf)
