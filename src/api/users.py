515d24bd7ff3b0b3efd0602995c6d337bb149640
dlabdouni <d.labdouni@gmail.com>
2023-10-10 14:44:33 +0200

    Update users.py

--- src/api/users.py
+++ src/api/users.py
@@ -41,3 +41,41 @@ def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic()))
             detail="Non Autorisé. Mauvais identifiant",
             headers={"WWW-Authenticate": "Basic"},
         )
+        
+        
+    
+# Fonction pour la vérification de l'identifiant et mot de passe de l'administrateur.
+def verify_credentials_admin(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    user = credentials.username
+    password = credentials.password
+
+    if not user or not password:
+        raise HTTPException(
+            status_code=401,
+            detail="Veuillez vous identifier en fournissant un identifiant et un mot de passe valides",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if user in users_db:
+        stored_password = users_db[user]
+        if password == stored_password:
+            if user == "admin":  # Vérification de l'utilisateur admin
+                return user
+            else:
+                raise HTTPException(
+                    status_code=403,
+                    detail="Accès refusé. Vous devez être un administrateur.",
+                    headers={"WWW-Authenticate": "Basic"},
+                )
+        else:
+            raise HTTPException(
+                status_code=401,
+                detail="Non autorisé. Mauvais mot de passe",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail="Non Autorisé. Mauvais identifiant",
+            headers={"WWW-Authenticate": "Basic"},
+        )
