a7b92600f35cfec27bf9285a23aa4d3991a1faf0
dlabdouni <d.labdouni@gmail.com>
2023-10-17 12:01:02 +0200

    Delete automation.py

--- src/api/automation.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Photon
-
-geolocator = Photon()
-addr = "Caxton Street, London, United Kingdom"
-location = geolocator.geocode(addr)
-
-print(location)
-
-print(location.raw)
