066e912298a1c2439068515836ba40ccf4d7b848
Willy <130974329+WillyMaillot87@users.noreply.github.com>
2023-10-19 16:33:33 +0200

    Update from branch Djamel

--- src/api_user/api_user.py
+++ src/api_user/api_user.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response
+from fastapi.responses import JSONResponse
 import uvicorn
 import pandas as pd
 from decouple import config
 import boto3
+import math
 
+from data.working_dataframe import working_dataframe
 from api.schema import NewCall
 from api.users import verify_credentials
+from api.fonction import format_time
 
 from joblib import load
 
@@ -30,11 +34,13 @@ s3_client = boto3.client('s3',region_name=config('AWS_S3_REGION'), aws_access_ke
 
 #Dictionnaire des codes d'erreur : 
 responses = {
-    200: {"description": "OK"},
-    401: {"description" : "Veuillez vous identifier"},
-    404: {"description": "Objet introuvable"},
-    403: {"description": "Accès restreint"},
-    406: {"description": "Mauvaise requête"}
+    "400": {"description": "Mauvaise requête"},
+    "401": {"description": "Non autorisé"},
+    "403": {"description": "Interdit"},
+    "404": {"description": "Non trouvé"},
+    "422": {"description": "Entité non traitable"},
+    "500": {"description": "Erreur interne du serveur"},
+    "503": {"description": "Service indisponible"},
 }
 
 
@@ -42,12 +48,12 @@ responses = {
 def check_api():
     return {"message": "L'API est fonctionnelle"}
 
-@app.get('/', tags=['Home'], name='Welcome', responses=responses)
+@app.post('/bienvenue', tags=['Home'], name='Welcome', responses={401: responses["401"]})
 async def get_index(current_user: str = Depends(verify_credentials)):
     """ 
     Message de bienvenue
     """
-    return {'message': f"Bonjour {current_user}. Bienvenue sur l'API du projet London Fire Brigade for users"}
+    return {'message': f"Bonjour {current_user}. Bienvenue sur le projet London Fire Brigade"}
 
 
 @app.post('/predict', tags=['Machine Learning'], name='predictions')
@@ -56,6 +62,14 @@ async def predict(new_call: NewCall):
     Obtenir une prédiction à partir de nouvelles données d'entrée.
     Les données d'entrée doivent être une instance de la class NewCall.
     """
+
+    # Vérifiez si les valeurs de NewCall ne correspondent pas aux valeurs de working_dataframe
+    invalid_values = {col: value for col, value in new_call.model_dump().items() if value not in working_dataframe[col].values}
+
+    if invalid_values:
+        error_message = f"Les valeurs suivantes ne sont pas valides : {', '.join(invalid_values)}"
+        return JSONResponse(content={"error": "Mauvaise Requête", "detail": error_message}, status_code=400)
+
     # Download des fichiers entrainés depuis AWS S3
     s3_client.download_file(Bucket=config('AWS_S3_BUCKET_NAME'), Key='lightgbm/model_lgb.joblib', Filename='models/model_lgb.joblib')
     s3_client.download_file(Bucket=config('AWS_S3_BUCKET_NAME'), Key='label_encoder.joblib', Filename='models/label_encoder.joblib')
@@ -64,7 +78,8 @@ async def predict(new_call: NewCall):
     loaded_model_lgb = load('models/model_lgb.joblib') # Chargement du modèle entrainé
     encoder = load('models/label_encoder.joblib') # Chargement du LabelEncoder ajusté aux données d'entrainement
     scaler = load('models/scaler_fitted.joblib') # Chargement du MinMaxScaler ajusté aux données d'entrainement
-    
+
+
     # Extraire les données
     input_data = pd.DataFrame([new_call.model_dump()])
     
@@ -79,9 +94,19 @@ async def predict(new_call: NewCall):
 
     # Faire une prédiction à partir du modèle :
     prediction = loaded_model_lgb.predict(scaled_data) 
+    prediction_in_seconds = prediction[0]
 
+    # Arrondir à la minute supérieure
+    rounded_seconds = round(prediction_in_seconds)
+    minutes = math.ceil(rounded_seconds / 60)
+
+    # Formater le temps en "X mins" (minute supérieure)
+    formatted_time = format_time(minutes)
+
+    response_text = f"Response time : {prediction_in_seconds} seconds. ({formatted_time})"
+        
     # Retourner la prédiction
-    return {"prediction": prediction[0]}
+    return Response(content=response_text, media_type="text/plain")
 
 
 if __name__ == '__main__':    
