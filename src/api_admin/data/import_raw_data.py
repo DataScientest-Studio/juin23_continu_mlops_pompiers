aa3bb46688ab9adaaed18c03cba4f79228503018
Willy <130974329+WillyMaillot87@users.noreply.github.com>
2023-10-24 11:25:07 +0200

    Update import_raw_data

--- src/api_admin/data/import_raw_data.py
+++ src/api_admin/data/import_raw_data.py
@@ -2,19 +2,29 @@ import mysql.connector
 from decouple import config
 
 connection = mysql.connector.connect(
-    host=config('DB_HOST'), # databse hébergée sur serveur AWS
+    host=config('DB_HOST'), # database hébergée sur serveur AWS
     user=config('DB_USER'),
     password=config('DB_PASSWORD'),
     database=config('DB_NAME')
 )
 
-# Création d'un curseur pour exécuter la requête SQL
+# Création d'un curseur pour exécuter les requêtes
 cursor = connection.cursor()
 
-# Exécution de la requête SQL dans MySQL
-query = """
-SELECT 
-    i.DateOfCall, i.HourOfCall, i.IncGeo_BoroughCode, i.IncGeo_WardCode, 
+# Requête DELETE pour supprimer les lignes où IncGeo_WardCode ou IncidentStationGround est vide
+delete_query = """
+DELETE FROM incident
+WHERE IncGeo_WardCode = '' OR IncidentStationGround = '';
+"""
+
+# Exécution de la requête DELETE
+cursor.execute(delete_query)
+connection.commit()
+
+# Requête SELECT pour récupérer les données
+select_query = """
+SELECT
+    i.DateOfCall, i.HourOfCall, i.IncGeo_BoroughCode, i.IncGeo_WardCode,
     i.Easting_rounded, i.Northing_rounded, i.IncidentStationGround,
     i.NumStationsWithPumpsAttending, i.NumPumpsAttending, i.PumpCount, i.PumpHoursRoundUp,
     m.PumpOrder, COALESCE(NULLIF(m.DelayCodeId, ''), 1) AS DelayCodeId, m.AttendanceTimeSeconds
@@ -22,8 +32,9 @@ FROM incident i
 RIGHT JOIN mobilisation m ON i.IncidentNumber = m.IncidentNumber
 WHERE i.DateOfCall IS NOT NULL AND i.PumpHoursRoundUp IS NOT NULL
 """
-cursor.execute(query)
 
+# Exécution de la requête SELECT
+cursor.execute(select_query)
 columns = [column[0] for column in cursor.description]
 
 # Récupération des données
